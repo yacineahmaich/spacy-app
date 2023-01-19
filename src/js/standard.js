f760d693172c43ce0962b4a8817cece374a6832a
Yacine Ahmaich <yacineahmich06@gmail.com>
2023-01-19 21:29:48 +0000

    regular commit

--- src/js/standard.js
+++ src/js/standard.js
@@ -1,33 +1,12 @@
-const sortBtn = document.querySelector(".sort__btn");
-const sortList = document.querySelector(".sort__list");
-const sortListItems = document.querySelectorAll(".sort__item");
-
-// Show / Hide the menu
-const toggleSortList = function () {
-  sortList.classList.toggle("show-menu");
-};
-
-// Select item
-sortListItems.forEach((item) => {
-  item.addEventListener("click", function () {
-    const icon = this.querySelector("use").getAttribute("href");
-    const label = this.querySelector("label").textContent;
-
-    sortBtn.querySelector("use").setAttribute("href", icon);
-    sortBtn.querySelector("label").textContent = label;
-    sortList.classList.remove("show-menu");
-  });
-});
-sortBtn.addEventListener("click", toggleSortList);
-
 // =============== Modal ===================
 
 const btnShowSearchMenu = document.querySelector(".quick-search__btn");
-const btnHideSearchMenu = document.querySelector(".popup__btn--close");
-const popup = document.querySelector(".popup");
+const btnHideSearchMenu = document.querySelector(".modal__btn--close");
+const popup = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
 const showSeachMenu = function () {
+  console.log("show");
   overlay.classList.remove("hidden");
   popup.classList.remove("hidden");
 };
